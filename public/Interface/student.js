0bed938dcd22b5b4b04ef4640e3f3bf1d1734d18
SiddarthBaruah <95840068+SiddarthBaruah@users.noreply.github.com>
2023-06-11 16:11:06 +0530

    Delete public directory

--- public/Interface/student.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3R1ZGVudC5qcyIsInNvdXJjZVJvb3QiOiIuL3NyYy8iLCJzb3VyY2VzIjpbIkludGVyZmFjZS9zdHVkZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
